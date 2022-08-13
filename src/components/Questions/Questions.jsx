be1df7873aac128a26c947eaed1f307dedd8aa5a
alijeyrad <alijeyrad@gmail.com>
2022-08-14 02:30:02 +0430

    pagination and submit button

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,7 +1,11 @@
-import React, { useState, useEffect, useContext } from 'react';
+/* eslint-disable max-len */
+import React, {
+  useState, useEffect, useContext,
+} from 'react';
 import Spinner from '../Spinner/Spinner';
 import AppContext from '../../contexts/AppContext';
 import SingleQuestion from '../SingleQuestion/SingleQuestion';
+import './Questions.css';
 
 function Questions() {
   const [load, setLoad] = useState(false);
@@ -24,6 +28,13 @@ function Questions() {
     document.documentElement.scrollTop = 0;
   }
 
+  // function handlePage(event) {
+  //   const num = event.target.innerText;
+  //   setPage(parseInt(num, 10));
+  //   document.body.scrollTop = 0;
+  //   document.documentElement.scrollTop = 0;
+  // }
+
   const { questions } = useContext(AppContext);
 
   const qArray = questions.filter(
@@ -37,24 +48,53 @@ function Questions() {
       )}
 
       {load && (
-      <div className="w3-animate-left">
-        <div className="w3-row-padding w3-padding">
-          <div className="w3-content w3-light-gray w3-padding-16 w3-card" style={{ maxWidth: '700px' }}>
-            {/* <div className="w3-header w3-light-grey w3-padding" style={{ zIndex: '5' }}>
-              <h2 className="w3-bar">Hi</h2>
-            </div> */}
-            <div className="w3-container" style={{ minHeight: '460px', zIndex: '1' }}>
-              {qArray.map((item) => (
-                <SingleQuestion ques={item} key={item.code} />
-              ))}
-            </div>
-            <div className="w3-bar w3-round w3-large" style={{ padding: '0 17px' }}>
-              <button onClick={handlePrev} type="button" className="w3-button">&#10094; Previous</button>
-              <button onClick={handleNext} type="button" className="w3-button w3-right">Next &#10095;</button>
+        <>
+          <div className="w3-animate-left">
+            <div className="w3-row-padding w3-padding">
+              <div className="w3-content w3-light-gray w3-padding-16" style={{ maxWidth: '700px', marginBottom: '50px' }}>
+                <div className="w3-container" style={{ minHeight: '460px', zIndex: '1' }}>
+                  {qArray.map((item) => (
+                    <SingleQuestion ques={item} key={item.code} />
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+
+          <div className="w3-animate-left w3-center">
+            <div
+              className="w3-row-padding"
+              style={{
+                zIndex: '1', position: 'fixed', bottom: '0', width: '100%', padding: '0 16px',
+              }}
+            >
+              <div className="w3-content w3-blue-gray w3-padding-16" style={{ maxWidth: '700px', opacity: '0.97' }}>
+                <div className="w3-container" style={{ zIndex: '1' }}>
+                  <div className="pagination">
+                    <button type="button" onClick={handlePrev} className="w3-button w3-large w3-round">&laquo;</button>
+                    <select className="w3-select w3-round" name="page" id="page" style={{ width: '30%' }}>
+                      <option value={`${page * 5}` - `${(page - 1) * 5}`} selected>
+                        {((page - 1) * 5) + 1}
+                        {' '}
+                        -
+                        {(page * 5)}
+                      </option>
+                      <option value={`${page * 5}` - `${(page - 1) * 5}`}>
+                        {((page - 1) * 5) + 1}
+                        {' '}
+                        -
+                        {(page * 5)}
+                      </option>
+                    </select>
+                    <button type="button" onClick={handleNext} className="w3-button w3-large w3-round">&raquo;</button>
+                    <button type="button" onClick={handleNext} className="submitButton w3-button w3-large w3-round w3-light-gray w3-wide">SUBMIT</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </>
       )}
     </>
   );
